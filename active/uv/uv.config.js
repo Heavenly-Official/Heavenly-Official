8eab351af38dc4d47fa6aca0b06fbc8c4d84e3c6
DevvyE_yo <DevEchoX@gmail.com>
2026-05-04 20:01:31 -0400

    Update bare URL in uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare: 'https://bare.noahmp.com/',
+    bare: ' https://ghostinc.online/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
